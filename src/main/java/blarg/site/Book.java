bc47b6b517ad140f1e5de54d0adee92f80e016db
cymrucoder <git.cmt@gmail.com>
2019-05-04 13:07:29 +0100

    Add book model and use to generate page.

---
+++ src/main/java/blarg/site/Book.java
@@ -0,0 +1,39 @@
+package blarg.site;
+
+/**
+ *
+ * @author cymrucoder
+ */
+public class Book {
+
+    private String title;
+    private String author;
+    private String color;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+ 
+
+}
--- src/main/java/blarg/site/BookController.java
+++ src/main/java/blarg/site/BookController.java
@@ -15,9 +15,20 @@ public class BookController {
 
     @GetMapping("/books")
     public String books(Model model) {
-        List<String> bookList = new ArrayList<>();
-        bookList.add("War of the Worlds");
-        bookList.add("Slaughterhouse Five");
+        List<Book> bookList = new ArrayList<>();
+        
+        Book book1 = new Book();
+        book1.setTitle("War of the Worlds");
+        book1.setAuthor("H G Wells");
+        book1.setColor("red");
+        
+        Book book2 = new Book();
+        book2.setTitle("Of Mice and Men");
+        book2.setAuthor("John Steinbeck");
+        book2.setColor("lightblue");
+        
+        bookList.add(book1);
+        bookList.add(book2);
         model.addAttribute("books", bookList);
         return "books";
     }
